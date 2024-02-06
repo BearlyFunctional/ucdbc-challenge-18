517e4ec7995e95480920f0db2d377ba144470ee7
BearlyFunctional <hjbell123.hb@gmail.com>
2024-02-05 17:48:06 -0800

    Added routes and controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,44 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getThoughtById(req, res) {
+        try {
+            const thought = await Thought.findById(req.params.id);
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findByIdAndUpdate(req.params.id, req.body, { new: true});
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findByIdAndDelete(req.params.id);
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
